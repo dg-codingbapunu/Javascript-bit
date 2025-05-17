55ff29c1f838e62117401d8c0b9a114871e61581
patitapaban <patitapabanpanda046@gmail.com>
2025-05-17 11:39:51 +0530

    Promise

--- index.js
+++ index.js
@@ -42,15 +42,35 @@
 
 // Call Back Function
 
-const firstFun = (name, callback) => {
-  setTimeout(() => {
-    console.log(`my name is ${name} `);
-    callback();
-  }, 3000);
-};
+// const firstFun = (name, callback) => {
+//   setTimeout(() => {
+//     console.log(`my name is ${name} `);
+//     callback();
+//   }, 3000);
+// };
 
-const secondFun = () => {
-  console.log("panda");
-};
+// const secondFun = () => {
+//   console.log("panda");
+// };
 
-firstFun("Patitapaban", secondFun);
+// firstFun("Patitapaban", secondFun);
+
+// Promises
+
+let promise = new Promise(function (resolve, reject) {
+  let condition = true;
+
+  if (condition == true) {
+    resolve("This is resolved");
+  } else {
+    reject("This is rejected");
+  }
+});
+
+promise.then((res) => {
+  console.log("Sucsess :", res);
+});
+
+promise.catch((err) => {
+  console.log("Error", err);
+});
